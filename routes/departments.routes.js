fca1a7cabe5ad03b9667996ea8518510cab8e69e
Krzysztof Potocki <kpotocki89@gmail.com>
2022-08-27 17:32:59 +0200

    Add Employee and Product model

--- routes/departments.routes.js
+++ routes/departments.routes.js
@@ -33,8 +33,8 @@ router.get("/departments/:id", async (req, res) => {
 });
 
 router.post("/departments", async (req, res) => {
+  const { name } = req.body;
   try {
-    const { name } = req.body;
     const newDepartment = new Department({ name: name });
     await newDepartment.save();
     res.json({ message: "OK" });
@@ -45,7 +45,6 @@ router.post("/departments", async (req, res) => {
 
 router.put("/departments/:id", async (req, res) => {
   const { name } = req.body;
-
   try {
     const dep = await Department.findById(req.params.id);
     if (dep) {
